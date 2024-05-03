c7ef7f204d562a75849524f5ca25989bee88b637
Adam Grabski <adam.gr@outlook.com>
2024-05-03 21:36:42 +0200

    stuff

--- Concrete.Extensions.Quizes/QuizTemplateData.cs
+++ Concrete.Extensions.Quizes/QuizTemplateData.cs
@@ -1,6 +1,8 @@
-﻿namespace Concrete.Extensions.Quizes;
+﻿using Concrete.Extensions.Quizes.Questions.Template;
+
+namespace Concrete.Extensions.Quizes;
 
 public class QuizTemplateData
 {
-
+	public List<QuestionTemplate> Questions { get; set; } = [];
 }
