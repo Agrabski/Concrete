9a7211470bfdf4ac17f117782b9b30ae3dbf75b8
Adam Grabski <adam.gr@outlook.com>
2024-05-04 22:38:45 +0200

    stuff

---
+++ Concrete.Core.Data/Migrations/20240504144915_AddExtensionDataStorage.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Concrete.Core.Data.Migrations;
+
+/// <inheritdoc />
+public partial class AddExtensionDataStorage : Migration
+{
+	/// <inheritdoc />
+	protected override void Up(MigrationBuilder migrationBuilder)
+	{
+		migrationBuilder.CreateTable(
+			name: "ExtensionData",
+			columns: table => new
+			{
+				Key = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+				ExtensionName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+				Category = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+				Value = table.Column<string>(type: "nvarchar(max)", nullable: false)
+			},
+			constraints: table => table.PrimaryKey("PK_ExtensionData", x => new { x.Key, x.ExtensionName }));
+
+		migrationBuilder.CreateIndex(
+			name: "IX_ExtensionData_Category",
+			table: "ExtensionData",
+			column: "Category");
+	}
+
+	/// <inheritdoc />
+	protected override void Down(MigrationBuilder migrationBuilder)
+	{
+		migrationBuilder.DropTable(
+			name: "ExtensionData");
+	}
+}
--- Concrete.Extensions.Quizes/QuizTemplateData.cs
+++ Concrete.Extensions.Quizes/QuizTemplateData.cs
@@ -1,4 +1,5 @@
-﻿using Concrete.Extensions.Quizes.Questions.Template;
+﻿using Concrete.Extensions.Quizes.Questions;
+using Concrete.Extensions.Quizes.Questions.Template;
 using System.Text.Json.Serialization;
 
 namespace Concrete.Extensions.Quizes;
@@ -13,15 +14,16 @@ public class QuizTemplateData
 [JsonDerivedType(typeof(SpecificQuestion), "specific")]
 public interface IQuizTemplateQuestionRefernce
 {
-
+	public decimal MaxScore { get; set; }
 }
 
 public class AnyQuestionWithTag : IQuizTemplateQuestionRefernce
 {
-
+	public required QuestionTag Tag { get; set; }
+	public decimal MaxScore { get; set; }
 }
 
 public class SpecificQuestion : IQuizTemplateQuestionRefernce
 {
-
+	public decimal MaxScore { get; set; }
 }
